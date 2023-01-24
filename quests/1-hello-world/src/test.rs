7ac209bcb81581c139f691f06f2ec3626c15325d
Mimir <alex@telluscoop.com>
2023-01-24 14:14:56 -0500

    01 test.rs -> spanish

--- quests/1-hello-world/src/test.rs
+++ quests/1-hello-world/src/test.rs
@@ -3,21 +3,21 @@
 use super::*;
 use soroban_sdk::{symbol, vec, Env};
 
-// The purpose of this file is to run automated tests on the contract code we've
-// written in `lib.rs`. Writing tests can be quite a big topic, and we'll dive
-// in further in a future quest. Just you wait!
+// El propósito de este archivo es ejecutar pruebas automatizadas en el código de contrato 
+// que hemos escrito en lib.rs. Escribir pruebas puede ser un tema bastante grande y 
+// profundizaremos más en una misión a futuro. ¡Atento!
 #[test]
 fn test() {
-    // we register the contract in a Soroban environment, and build a client we
-    // can use to invoke the contract
+    // registramos el contrato en un entorno (environment) Soroban y construimos 
+    // un cliente que podamos usar para invocar el contrato
     let env = Env::default();
     let contract_id = env.register_contract(None, HelloContract);
     let client = HelloContractClient::new(&env, &contract_id);
 
-    // Next, we call `client.hello()`, supplying "Dev" as our `to` argument.
+    // A continuación, llamamos a `client.hello()`, proporcionando "Dev" como nuestro argumento `to`.
     let words = client.hello(&symbol!("Dev"));
     
-    // We assert the contract must return a Vec that matches what we would
-    // expect to receive from our contract: [Symbol("Hello"), Symbol("Dev")]
+    // Afirmamos que el contrato debe devolver un Vec que coincida 
+    // con lo que esperaríamos recibir de nuestro contrato: [Symbol("Hello"), Symbol("Dev")]
     assert_eq!(words, vec![&env, symbol!("Hello"), symbol!("Dev"),]);
 }
